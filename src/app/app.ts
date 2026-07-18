34c02843f53144dd5cd1e8f6837867e1334f5223
mrbunroeun <bunroeunhas@gmail.com>
2026-07-18 22:20:28 +0700

    cooking

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <nav class="flex gap-4 p-4">
+      <a routerLink="/">Home</a>
+      <a routerLink="/ohio2">Ohio2</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('my-first-angular-app');
---
+++ src/app/ohio2/ohio2.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ohio2',
+  standalone: true,
+  imports: [],
+  templateUrl: './ohio2.component.html',
+})
+export class Ohio2Component {}
