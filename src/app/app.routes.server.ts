02ab19be1d55aa654b543e546a3c15ce47ff3642
mrbunroeun <bunroeunhas@gmail.com>
2026-08-03 14:44:27 +0700

    pushing to vercel

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'latest-events-detail/:id',
+    renderMode: RenderMode.Server, // render per-request instead of prerendering
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
